528bbf6775a5b7fd0f5ceec347f5d5946e35552a
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-05-24 00:36:54 +0530

    incomplete solution for problem F

--- codeforces/div-3-874/src/bin/f.rs
+++ codeforces/div-3-874/src/bin/f.rs
@@ -54,18 +54,81 @@ fn main() {
         let mut unique_values: Vec<_> = a.into_iter().collect::<HashSet<_>>().into_iter().collect();
         let mut ans = 0;
         unique_values.sort();
-
-        for val in unique_values {
-            let mut prod: u64 = 1;
-            for j in val..(val + m as u32) {
-                if let Some(value) = frequencies.get(&j) {
-                    prod *= *value as u64;
-                } else {
-                    prod = 0;
-                }
-                prod %= MOD;
+        let mut prefix_frequencies = vec![0u64; n + 1];
+        for (id, val) in unique_values.iter().enumerate() {
+            prefix_frequencies[id] = if let Some(value) = frequencies.get(val) {
+                *value as u64 % MOD
+            } else {
+                0
+            };
+            if id > 0 {
+                prefix_frequencies[id] *= prefix_frequencies[id - 1];
+                prefix_frequencies[id] %= MOD;
             }
-            ans += prod;
+        }
+
+        let get_index = |r: u32| -> Option<usize> {
+            let mut lo = 0;
+            let mut hi = unique_values.len() - 1;
+            let mut ans: i32 = -1;
+
+            while lo <= hi {
+                let mid = (lo + hi) / 2;
+                if unique_values[mid] <= r {
+                    ans = mid as i32;
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+            if ans >= 0 {
+                Some(ans as usize)
+            } else {
+                None
+            }
+        };
+
+        fn power(mut a: u64, mut b: u64) -> u64 {
+            let mut ans = 1;
+            while b > 0 {
+                if b % 2 == 1 {
+                    ans = ans * a % MOD;
+                }
+                a = a * a % MOD;
+                b /= 2;
+            }
+            ans
+        }
+
+        fn inverse_mod(val: u64) -> u64 {
+            power(val, MOD - 2)
+        }
+
+        let get_answer = |l: u32, r: u32| -> u64 {
+            let mut prod: u64 = 0;
+            if let Some(idr) = get_index(r) {
+                prod = prefix_frequencies[idr];
+                if let Some(idl) = get_index(l) {
+                    if idl >= 1 {
+                        prod *= inverse_mod(prefix_frequencies[idl - 1]);
+                        prod %= MOD;
+                    }
+                }
+            }
+            prod
+        };
+
+        for val in &unique_values {
+            // let mut prod: u64 = 1;
+            // for j in val..(val + m as u32) {
+            //     if let Some(value) = frequencies.get(&j) {
+            //         prod *= *value as u64;
+            //     } else {
+            //         prod = 0;
+            //     }
+            //     prod %= MOD;
+            // }
+            ans += get_answer(*val, val + m as u32 - 1);
             if ans >= MOD {
                 ans -= MOD;
             }
