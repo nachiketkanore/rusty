8810f304fc5e6487e2466506746e0000c51954ab
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-05-23 19:43:50 +0530

    solved problem c

---
+++ codeforces/div-3-874/src/bin/c.rs
@@ -0,0 +1,51 @@
+use std::io::{stdin, stdout, BufWriter, Write};
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>,
+}
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+
+fn main() {
+    let mut scan = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+
+    let tcs: usize = scan.next();
+    for _ in 0..tcs {
+        let n: usize = scan.next();
+        let mut even = 0;
+        let mut odd = 0;
+        let mut a = Vec::new();
+        for _ in 0..n {
+            let val: i32 = scan.next();
+            a.push(val);
+            if val % 2 == 1 {
+                odd += 1;
+            } else {
+                even += 1;
+            }
+        }
+        if even == n || odd == n {
+            writeln!(out, "YES").unwrap();
+        } else {
+            let smallest_even: i32 = *a.iter().filter(|&val| val % 2 == 0).min().unwrap();
+            let smallest_odd: i32 = *a.iter().filter(|&val| val % 2 == 1).min().unwrap();
+            if smallest_even > smallest_odd {
+                writeln!(out, "YES").unwrap();
+            } else {
+                writeln!(out, "NO").unwrap();
+            }
+        }
+    }
+}
